caa5456405ef291cb6b2dd5b9cc42912d972f1b7
anviar <oleg.kluchkin+github@gmail.com>
2016-08-04 17:34:51 +0300

    Direct backup
    Direct backup to S3 without local staging

--- s3_simple/backup.py
+++ s3_simple/backup.py
@@ -11,12 +11,12 @@ workdir=config.get('general', 'workdir')
 date=time.strftime("%Y-%m-%d")
 incfile=workdir+"/files.snar"
 
-transfer = S3Transfer( boto3.client(
+s3 = boto3.client(
 			's3',
 			aws_access_key_id=config.get('amazon', 'aws_access_key_id'),
 			aws_secret_access_key=config.get('amazon', 'aws_secret_access_key'),
 			region_name=config.get('amazon', 'region')
-		))
+		)
 
 if ( len(sys.argv) > 1 and sys.argv[1] == "-f" ) or not os.path.isfile(incfile):
 	mode="full"
@@ -32,7 +32,7 @@ class filesThread(threading.Thread):
 		print "%s: started %s - %s" % (time.strftime('%Y-%m-%d %H:%M:%S'),self.name,mode)
 		paths=config.get('files','paths').split(',')
 		exclude=config.get('files','exclude').split(',')
-		tar_comm=["tar","-zcf",workdir+'/files.tar.gz','--listed-incremental='+incfile]
+		tar_comm=["tar","-zcf",'-','--listed-incremental='+incfile]
 		if mode == "full":
 			tar_comm.append('--level=0')
 		for x in exclude:
@@ -40,10 +40,10 @@ class filesThread(threading.Thread):
 		for p in paths:
 			tar_comm.append(p)
 		devnull = open(os.devnull, 'w')
-		subprocess.call(tar_comm,stderr=devnull)
+		#subprocess.call(tar_comm,stderr=devnull)
+		with subprocess.Popen(tar_comm, stdout=subprocess.PIPE, stderr=devnull).stdout as dataStream:
+			s3.upload_fileobj(dataStream, config.get('amazon', 'bucket'), 'files_' + mode + '_' + date + '.tar.gz' )
 		devnull.close()
-		print "%s: files saved on disk" % time.strftime('%Y-%m-%d %H:%M:%S')
-		transfer.upload_file(workdir+'/files.tar.gz', config.get('amazon', 'bucket'), 'files_'+mode+'_'+date+'.tar.gz' )
 		print "%s: files uploaded" % time.strftime('%Y-%m-%d %H:%M:%S')
 
 class mysqlThread(threading.Thread):
@@ -52,26 +52,17 @@ class mysqlThread(threading.Thread):
                 self.daemon = False
                 self.name = "mysql-backup"
         def run(self):
-                print "%s: started %s " % (time.strftime('%Y-%m-%d %H:%M:%S'),self.name)
-		databases=config.get('mysql', 'databases').split(',')
-		dbuser=config.get('mysql', 'user')
-		dbpass=config.get('mysql', 'password')
-		for db in databases:
-			sql_file=open(workdir+'/'+db+".sql.bz2", "wb+")
-			mysql_dump=subprocess.Popen(["mysqldump","-u"+dbuser,"-p"+dbpass,"--add-drop-table",db], stdout=subprocess.PIPE)
-			mysql_compressor=subprocess.Popen("bzip2", stdin=mysql_dump.stdout, stdout=sql_file)
-			mysql_dump.stdout.close()
-			mysql_compressor.communicate()
-			sql_file.close()
-			print "%s: %s database saved on disk" % (time.strftime('%Y-%m-%d %H:%M:%S'),db)
-		for db in databases:
-			transfer.upload_file(workdir+'/'+db+".sql.bz2", config.get('amazon', 'bucket'), "mysql_"+db+'_'+date+".bz2" )
-			print "%s: %s database uploaded" % (time.strftime('%Y-%m-%d %H:%M:%S'),db)
-
+			print "%s: started %s " % (time.strftime('%Y-%m-%d %H:%M:%S'),self.name)
+			databases=config.get('mysql', 'databases').split(',')
+			dbuser=config.get('mysql', 'user')
+			dbpass=config.get('mysql', 'password')
+			for db in databases:
+				with subprocess.Popen( "mysqldump -u"+ dbuser +" -p" + dbpass + " --add-drop-table " + db + "|bzip2", stdout=subprocess.PIPE, shell=True).stdout as dataStream:
+					s3.upload_fileobj(dataStream, config.get('amazon', 'bucket'), "mysql_" + db + '_' + date + ".bz2" )
+				print "%s: %s database saved to cloud" % (time.strftime('%Y-%m-%d %H:%M:%S'),db)
 
 mysql_t=mysqlThread()
 files_t=filesThread()
 
 files_t.start()
 mysql_t.start()
-
